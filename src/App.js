a4cc2f00d77d7444b080e9debe507d13f958adbd
Juliet Nnaji <98786226+JulietNnaji@users.noreply.github.com>
2022-08-01 16:26:01 +0100

    Created a new react repository for the weather app

--- src/App.js
+++ src/App.js
@@ -4,20 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Weather App</h1>
+     <footer className="owner">
+          <a href="https://github.com/JulietNnaji/react-weather-app" target="blank">
+            Open-source code </a
+          >
+          by Juliet Nnaji
+        </footer>
     </div>
   );
 }
