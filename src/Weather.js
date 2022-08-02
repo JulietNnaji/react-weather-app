4549e2d095a46623ff85cd8fd12e3763654b99d9
Juliet Nnaji <98786226+JulietNnaji@users.noreply.github.com>
2022-08-02 16:02:24 +0100

    Added my API integration

--- src/Weather.js
+++ src/Weather.js
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useState} from "react";
 import './Weather.css';
 import "bootstrap/dist/css/bootstrap.css";
+import axios from "axios";
+import { Audio } from  'react-loader-spinner';
 
 export default function Weather (){
-    return (
+  let [loaded, setLoader] = useState (false);
+  let [values, setValues] =useState(null);
+  function showTemperature(response){
+    console.log(response.data);
+setValues({
+ temperature: response.data.main.temp,
+ humidity: response.data.main.humidity,
+ city: response.data.name,
+ wind: response.data.wind.speed,
+ pressure: response.data.main.pressure,
+ date: " Tues, 2 Aug; 13:19",
+ description: response.data.weather[0].description,
+ weatherIcon: `http://openweathermap.org/img/wn/${
+  response.data.weather[0].icon
+}@2x.png`
+});
+setLoader (true);
+  } 
+  
+if (loaded){
+  return (
         <div className="Weather">
         <div className ="Wrapper">
                 <div className ="formName"><form>
@@ -19,29 +41,29 @@ export default function Weather (){
           </div>
           </form>
           
-            <h1 className ="mt-3">Lagos</h1>
+            <h1 className ="mt-3 text-capitalize">{values.city}</h1>
          
           </div>
-          <img src= "https://ssl.gstatic.com/onebox/weather/48/sunny.png" className="image" alt="sunny" />
+          <img src= {values.weatherIcon} className="image" alt={values.description} />
           <div className="position">
-            <strong className ="degree">  69 </strong>
+            <strong className ="degree">  {Math.round (values.temperature)} </strong>
           <span className="temperature">°C | °F </span> {""}
-            <span className ="description">
-            Partly Cloudy
+            <span className ="description text-capitalize">
+            {values.description}
             </span>
           </div>
           <ul className="lists mt-3">
             <li>
-             Last updated:  Tues, 2 Aug; 13:19
+             Last updated: {values.date}
             </li>
             <li>
-           Humidity: 24%
+           Wind: {Math.round(values.wind)}km/h
             </li>
             <li>
-           Wind: 84 km/h
+           Humidity: {values.humidity}%
             </li>
             <li>
-           Precipitation: 2%
+           Pressure: {values.pressure}hPa
             </li>
           </ul>
           </div>
@@ -53,4 +75,22 @@ export default function Weather (){
             </footer>
         </div>
       );
-    }
+    
+}else{
+    let apiKey = "0bef54fbf2f25ff0f6335be419abd9a2";
+    let city = "new york";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(showTemperature);
+  
+    return (<p> 
+    <Audio
+        height = "80"
+        width = "1000"
+        radius = "9"
+        color = 'green'
+        ariaLabel = 'three-dots-loading'     
+        wrapperStyle
+        wrapperClass
+      /></p> );
+}  
+  }
