e0109bfc0280e1b683d7a9d56ba4c1480da38a1d
Juliet Nnaji <98786226+JulietNnaji@users.noreply.github.com>
2022-08-02 13:33:34 +0100

    Added basic styling

--- src/Weather.js
+++ src/Weather.js
@@ -6,35 +6,42 @@ export default function Weather (){
     return (
         <div className="Weather">
         <div className ="Wrapper">
-                <form className ="formName">
+                <div className ="formName"><form>
           <div className ="row">
-            <div className = "col-4">
-           <input type ="search" placeholder="Search for a city..." className ="form"/>
+            <div className = "col-6">
+           <input type ="search" placeholder="Search for a city..." className ="ms-4"/>
             </div>
-            <div className ="col-2">
-            <h1>Lagos</h1>
-            </div>
-            <div className ="col-2">
+        
+            <div className ="col-6">
                 <input type ="submit"
-                value ="Search" className ="btn btn-primary" />
+                value ="Search" className ="btn btn-primary w-50" />
             </div>
           </div>
           </form>
-          <img src= "https://ssl.gstatic.com/onebox/weather/48/sunny.png" alt="sunny" />
-          <div className ="degree">  69°C {""}
-            <span>
+          
+            <h1 className ="mt-3">Lagos</h1>
+         
+          </div>
+          <img src= "https://ssl.gstatic.com/onebox/weather/48/sunny.png" className="image" alt="sunny" />
+          <div className="position">
+            <strong className ="degree">  69 </strong>
+          <span className="temperature">°C | °F </span> {""}
+            <span className ="description">
             Partly Cloudy
             </span>
           </div>
-          <ul>
+          <ul className="lists mt-3">
             <li>
-             Date: July 2 2022
+             Last updated:  Tues, 2 Aug; 13:19
             </li>
             <li>
            Humidity: 24%
             </li>
             <li>
            Wind: 84 km/h
+            </li>
+            <li>
+           Precipitation: 2%
             </li>
           </ul>
           </div>
